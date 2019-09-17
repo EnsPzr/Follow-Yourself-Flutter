691469271410bba2f8bdb8bda0848c6a44fe9649
Mehmet Enes PAZAR <enespazar4342@gmail.com>
2019-09-17 21:05:52 +0300

    project design manufacturing 2

--- lib/pages/activiy_add_or_update_page.dart
+++ lib/pages/activiy_add_or_update_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'drawer_menu.dart';
-
-class ActivityAddOrUpdatePage extends StatelessWidget {
+class ActivityAddOrUpdatePage extends StatefulWidget {
   const ActivityAddOrUpdatePage({Key key}) : super(key: key);
 
+  @override
+  _ActivityAddOrUpdatePageState createState() =>
+      _ActivityAddOrUpdatePageState();
+}
+
+class _ActivityAddOrUpdatePageState extends State<ActivityAddOrUpdatePage> {
+  final _formKey = GlobalKey<FormState>();
+  bool secilenDurum = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +19,49 @@ class ActivityAddOrUpdatePage extends StatelessWidget {
         title: Text("Aktivite Ekle"),
         backgroundColor: Colors.red.shade900,
       ),
-      body: Text("ekleme sayfası"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              Text(
+                "Aktivite Adı:",
+                style: TextStyle(color: Colors.blue.shade700),
+              ),
+              TextFormField(
+                decoration: InputDecoration(),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Aktivite Aktiflik Durumu:",
+                style: TextStyle(color: Colors.blue.shade700),
+              ),
+              DropdownButton<bool>(
+                hint: Text("Seçiniz"),
+                value: secilenDurum,
+                items: [
+                  DropdownMenuItem<bool>(
+                    child: Text("Aktif Değil"),
+                    value: false,
+                  ),
+                  DropdownMenuItem<bool>(
+                    child: Text("Aktif"),
+                    value: true,
+                  ),
+                ],
+                onChanged: (bool deger) {
+                  setState(() {
+                    secilenDurum = deger;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
